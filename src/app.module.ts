bd0eb1036c24a801984f561207bf0d3314ff35be
Zipeth <markchristensen1010@gmail.com>
2023-05-30 12:02:29 +0100

    descriptions and image urls added to breeds

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DogBreedsController } from './dog-breeds.controller';
 import { DogBreedsService } from './dog-breeds.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, DogBreedsController],
-  providers: [AppService, DogBreedsService],
+  controllers: [DogBreedsController],
+  providers: [DogBreedsService],
 })
 export class AppModule {}
--- src/dog-breeds.service.ts
+++ src/dog-breeds.service.ts
@@ -2,12 +2,31 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DogBreedsService {
-  getAllBreeds(): string[] {
-    // Replace this with your logic to retrieve the list of dog breeds
-    const breeds: string[] = [
-      'Labrador Retriever',
-      'German Shepherd',
-      'Golden Retriever',
+  getAllBreeds(): { breed: string; description: string; image: string }[] {
+    const breeds: { breed: string; description: string; image: string }[] = [
+      {
+        breed: 'Labrador Retriever',
+        description: 'The cuddliest and woofiest of the lot',
+        image:
+          'https://www.thelabradorsite.com/wp-content/uploads/2017/07/When-Do-Dogs-Stop-Growing-LS-long.jpg',
+      },
+      {
+        breed: 'German Shepherd',
+        description: 'The smartest and scariest of the lot',
+        image:
+          'https://media.zenfs.com/en/pethelpful_915/25a694c61991611ab32d49a41b3eb4cf',
+      },
+      {
+        breed: 'Golden Retriever',
+        description: 'The most energetic and playful of the lot',
+        image:
+          'https://global-uploads.webflow.com/59dbe1c3542805000192616b/63178a7970d1b57a4c6a7a05_golden-retriever.png',
+      },
+      {
+        breed: 'Pug',
+        description: 'The least intelligent and loudest of the lot',
+        image: 'https://wallpapercave.com/wp/QbA40M5.jpg',
+      },
     ];
     return breeds;
   }
